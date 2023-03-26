392f23591752662a1b08016175b6a8044f4da521
kbar163 <kabarrantes163@gmail.com>
2023-03-26 16:35:24 -0600

    Express.js hello world, added gitignore

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const http = require('http');
 
-const routes = require('./routes')
+const express = require('express');
 
-const server = http.createServer(routes);
-server.listen(5000);
+const app = express();
+
+//Middlewares
+app.use((req, res, next) => {
+    console.log("In the first middleware!");
+    next(); //Allows the request to continue to the next middlewate in line
+});
+
+app.use((req, res, next) => {
+    console.log("In the second middleware!");
+    res.send('<h1>Hello world from express!</h1>');
+});
+
+const server = http.createServer(app);
+server.listen(5000); 
