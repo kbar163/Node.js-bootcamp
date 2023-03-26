86dc8ff1b933aa07db1f14edce9ee5cc8eeb8442
kbar163 <kabarrantes163@gmail.com>
2023-03-25 19:16:00 -0600

    Variables and functions in JavaScript
    refresher on js. Variable declarations and simple functions syntax

--- first-app.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs'); //Importing file system
-
-fs.writeFileSync('hello.txt', 'Hello world');
---
+++ playground.js
@@ -0,0 +1,19 @@
+//Javascript refresher. Core functionalities/features.
+
+//variable declarations
+var myName = 'Kevin';
+var myAge = 27;
+var hasHobbies = true;
+
+//functions
+function userData(userName,userAge,userHasHobby) {
+    return (
+        'Name is ' + userName +
+        ', age is ' + userAge +
+        ' and the user has hobbies ' + userHasHobby
+    );
+}
+
+//console output
+console.log(userData('Kevin',27,true));
+
