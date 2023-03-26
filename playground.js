b2e70fd1a8c925a88efe8dc001760368b02773aa
kbar163 <kabarrantes163@gmail.com>
2023-03-25 20:43:14 -0600

    Async simple explanation

--- playground.js
+++ playground.js
@@ -1,37 +1,14 @@
+//Async Code
+//Let's do a simple example illustrating async code.
 
-//Object destructuring
-const person = {
-    name: 'Kevin',
-    age: 27,
-    greet() {
-        console.log("Hello! My name is " + this.name +
-        " and I\'m " + this.age +" years old.");
-    }
-};
+//This timeout function waits for 1 milisecond before executing
+//the specified callback function
+setTimeout(() => {
+    console.log("I'm third");
+},1);
 
-//Let's say we have a function that takes an specific object from
-//which we want to get a single key
-const getName = (object) => {
-    console.log(object.name);
-}
-
-
-
-//We can use the object destructuring syntax to make this more general
-const getName02 = ({ name }) => {
-    console.log(name)
-}
-
-//We can also create constants or variables using destructuring
-
-let {name, age} = person;
-console.log(name,age);
-
-//It's also possible to destructure arrays by position
-
-const exampleArray = ['football','basketball','baseball'];
-
-let [firstElement] = exampleArray;
-console.log(firstElement);
-
-
+//Since Node.js does not block the code execution waiting for the
+//timeout to happen, these prints will occur prior to the
+//execution of the function above
+console.log("I'm first")
+console.log("I'm second")
