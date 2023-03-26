7347885db8feba31c818e5080bba5da647d4f469
kbar163 <kabarrantes163@gmail.com>
2023-03-25 22:09:13 -0600

    Node.js server example
    Raw logic bare bones node.js web server that returns a simple html form on the '/' route and takes the user input of the form retrieved on the '/message' route and writes a file to save it.

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const http = require('http');
-
+const fs = require('fs');
 
 
 const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type','text/html');
-    res.write('<html><head><title>My first page</title></head><body><h1>Hello World</h1></body></html>');
-    res.end();
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.write('<html><head><title>My first page</title></head><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data',(chunk) => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        });
+
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end()
+    }
+
 });
 
 server.listen(5000);
