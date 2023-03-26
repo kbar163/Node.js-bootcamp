d750852013c0b292822c47441f35808f313a3d2f
kbar163 <kabarrantes163@gmail.com>
2023-03-25 21:37:45 -0600

    Vanilla node.js hello world page

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const http = require('http');
 
 
 const server = http.createServer((req,res) => {
-    console.log(req);
-    process.exit();
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html><head><title>My first page</title></head><body><h1>Hello World</h1></body></html>');
+    res.end();
 });
 
 server.listen(5000);
